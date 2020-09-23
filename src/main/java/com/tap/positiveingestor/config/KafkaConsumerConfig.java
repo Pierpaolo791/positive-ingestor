1ffa71bb04532fefa4286fc8f886cffce4441d6c
Pierpaolo Pecoraio <pierpaolo.pecoraio@gmail.com>
2020-09-23 11:20:21 +0200

    changed StringSerializer and Deserializer

--- src/main/java/com/tap/positiveingestor/config/KafkaConsumerConfig.java
+++ src/main/java/com/tap/positiveingestor/config/KafkaConsumerConfig.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +13,6 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 
-import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
 
 @EnableKafka
 @Configuration
